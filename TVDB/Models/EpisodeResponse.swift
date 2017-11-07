72e739eebe26c180eaf949c1d4a53d45bf9e4511
Pietro Caselani <pc1992@gmail.com>
2017-11-07 17:14:55 -0200

    Fix JSON parsing

--- TVDB/Models/EpisodeResponse.swift
+++ TVDB/Models/EpisodeResponse.swift
@@ -1,7 +1,13 @@
 public final class EpisodeResponse: Codable {
   public let episode: FullEpisode
 
-	enum CodingKeys: String, CodingKey {
+	private enum CodingKeys: String, CodingKey {
 		case episode = "data"
+	}
+
+	public required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+
+		self.episode = try container.decode(FullEpisode.self, forKey: .episode)
 	}
 }
--- TVDB/Models/FullEpisode.swift
+++ TVDB/Models/FullEpisode.swift
@@ -1,3 +1,13 @@
 public final class FullEpisode: Codable {
   public let filename: String?
+
+	private enum CodingKeys: String, CodingKey {
+		case filename
+	}
+
+	public required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+
+		self.filename = try container.decodeIfPresent(String.self, forKey: .filename)
+	}
 }

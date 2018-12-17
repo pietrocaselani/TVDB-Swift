0707b3b7d3baff0b4065004487d425eb78922865
Pietro Caselani <pc1992@gmail.com>
2018-12-16 23:05:50 -0200

    Fix project configuration to run the tests

--- TVDB/BundleExtensions.swift
+++ TVDB/BundleExtensions.swift
@@ -1,8 +1,8 @@
 extension Bundle {
 	static var testing: Bundle {
-		let bundle = Bundle(for: TVDB.self)
+        let bundle = Bundle(for: TVDB.self)
 
-		let path = bundle.bundlePath.appending("/../../../../TVDBTestsResources.bundle")
+		let path = bundle.bundlePath.appending("/../../TVDBTestsResources.bundle")
 		let testingBundle = Bundle(path: path)
 
 		return testingBundle!

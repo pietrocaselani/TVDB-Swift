dfb69e43f173469c6c2b52b84c9628c6b6903bcf
Pietro Caselani <pc1992@gmail.com>
2018-12-22 22:16:04 -0200

    Search for testing bundle

--- TVDB/BundleExtensions.swift
+++ TVDB/BundleExtensions.swift
@@ -2,9 +2,23 @@ extension Bundle {
 	static var testing: Bundle {
         let bundle = Bundle(for: TVDB.self)
 
-		let path = bundle.bundlePath.appending("/../../TVDBTestsResources.bundle")
-		let testingBundle = Bundle(path: path)
-
-		return testingBundle!
+		return findBundle(using: bundle.bundlePath)
 	}
+
+    private static func findBundle(using path: String) -> Bundle {
+        let bundleName = "TVDBTestsResources.bundle"
+        var upPath = "/.."
+
+        var attempts = 0
+        var testingBundle: Bundle?
+
+        repeat {
+            let fullPath = path.appending("\(upPath)/\(bundleName)")
+            testingBundle = Bundle(path: fullPath)
+            attempts += 1
+            upPath = upPath.appending(upPath)
+        } while testingBundle == nil && attempts <= 5
+
+        return testingBundle!
+    }
 }
